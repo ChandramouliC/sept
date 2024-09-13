40ec986328874565f19d76d34f088bd8bc3ba34f
chand <chandramoulichalla734@gmail.com>
2024-09-13 10:13:30 +0530

    Add files repository

---
+++ src/javapractice/practice002.java
@@ -0,0 +1,16 @@
+package javapractice;
+
+public class practice002 {
+    public static void main(String[] args) {
+        int n;
+        {
+            n=2;
+        }
+        System.out.println(n+n);
+        System.out.println(n-n);
+        System.out.println(n*n);
+        System.out.println(n/n);
+
+
+    }
+}
